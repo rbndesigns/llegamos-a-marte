031ec1b0a608c5b057a494a4c724b97bdb36eba7
A97-MIYAMOTO-\Alumno <0>
2025-11-12 19:39:56 +0100

    A

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var config = {
             }
         },
         color: {
-            value: ["#ffffff", "#ffffffff", "#F9F871", "#dfa02dff"]
+            value: ["#ffffff", "#FF6F91", "#F9F871", "#D65DB1"]
         },
         shape: {
             type: "circle",
